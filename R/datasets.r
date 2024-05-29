b48f27d29aa88995112da4bdb55e942e0180918e
Bernd.Gruber <Bernd.Gruber@canberra.edu.au>
2024-05-29 10:14:38 +1000

    dartR.data 1.0.5 added new data for dartR.sexlinked compressed data

--- R/datasets.r
+++ R/datasets.r
@@ -104,7 +104,7 @@ NULL
 #' @author Bernd Gruber (bugs? Post to 
 #' \url{https://groups.google.com/d/forum/dartr}
 #' @keywords datasets
-#' @examples
+#' @examples 
 #' \dontrun{
 #' library(PopGenReport)
 #' fp <- file.path(system.file( package="dartR.data"),"extdata/platy.csv")
@@ -115,5 +115,66 @@ NULL
 #' df.loc <- data.frame(RepAvg = runif(nLoc(platy.gl)), CallRate = 1)
 #' platy.gl@other$loc.metrics <- df.loc
 #' gl.report.reproducibility(platy.gl)
-#' }
+#' } 
+NULL
+
+#' Laedbeater's possum (LBP) DArT data in a genlight object
+#'
+#' Genetic data from LBP collected in VIC, Australia by researchers at Monash 
+#' University. The data comes in the form of a genlight object ready to be used 
+#' by dartR. The file contains data for 376 individuals from 2 populations and 
+#' 1,000 binary SNPs. The dataset contains a mix of autosomal and sex-linked
+#' loci.
+#' @name LBP
+#' @format genlight object
+#' @docType data
+#' @author Custodian: Diana Robledo-Ruiz -- Post to
+#'   \url{https://groups.google.com/d/forum/dartr}
+#' @keywords datasets
+#' @references Robledo-Ruiz et al. (2023) Easy-to-use R functions to separate 
+#' reduced-representation genomic datasets into sex-linked and autosomal loci, 
+#' and conduct sex assignment. \emph{Mol. Ecol. Res}.
+#' \url{https://doi.org/10.1111/1755-0998.13844}
+"LBP"
+
+#' Yellow-tufted honeyeater (YTH) DArT data in a genlight object
+#'
+#' Genetic data from YTH collected in VIC, Australia by researchers at Monash 
+#' University. The data comes in the form of a genlight object ready to be used 
+#' by dartR. The file contains data for 641 individuals from 8 populations and 
+#' 1,000 binary SNPs. The dataset contains a mix of autosomal and sex-linked
+#' loci.
+#'
+#' @name YTH
+#' @format genlight object
+#' @docType data
+#' @author Custodian: Diana Robledo-Ruiz -- Post to
+#'   \url{https://groups.google.com/d/forum/dartr}
+#' @keywords datasets
+#' @references Robledo-Ruiz et al. (2023) Easy-to-use R functions to separate 
+#' reduced-representation genomic datasets into sex-linked and autosomal loci, 
+#' and conduct sex assignment. \emph{Mol. Ecol. Res.}
+#' (\url{https://doi.org/10.1111/1755-0998.13844})
+"YTH"
+
+#' Eastern yellow robin (EYR) DArT data in a genlight object
+#'
+#' Genetic data from EYR collected in VIC, Australia by researchers at Monash 
+#' University. The data comes in the form of a genlight object ready to be used 
+#' by dartR. The file contains data for 782 individuals from 4 populations and
+#' 1,000 binary SNPs. The dataset contains a mix of autosomal and sex-linked
+#' loci.
+#'
+#' @name EYR
+#' @format genlight object
+#' @docType data
+#' @author Custodian: Diana Robledo-Ruiz -- Post to
+#'   \url{https://groups.google.com/d/forum/dartr}
+#' @keywords datasets
+#' @references Robledo-Ruiz et al. (2023) Easy-to-use R functions to separate 
+#' reduced-representation genomic datasets into sex-linked and autosomal loci, 
+#' and conduct sex assignment. \emph{Mol. Ecol. Res.}
+#' (\url{https://doi.org/10.1111/1755-0998.13844})
+#' @import adegenet
+"EYR"
 NULL
